af995aee4af9b46e4069d88102d92f31b710057c
martin Elias <martinelias@MacBook-Air-de-martin.local>
2022-07-15 15:25:54 +0200

    added api , with no protection

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -4,10 +4,17 @@ namespace App\Entity;
 
 use App\Repository\BookRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: BookRepository::class)]
+#[ApiResource()]
+
+
 class Book
 {
+    
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
